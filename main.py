1a39297a63b2104cd440d7154a132f37ca8c2076
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-04-24 09:35:08 -0300

    Application class and first frames

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+
 class Calculator:
     def __init__(self):
         self.operators = {
@@ -18,3 +20,40 @@ class Calculator:
     
     def divide(self, a: float, b: float) -> float:
         return a / b
+
+
+class Application(Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.buttons = []
+
+        self.display_frame = Frame(self, bg='#999999', width=400, height=75)
+        self.numeric_keys_frame = Frame(self, bg="#FCFF67", width=200, height=250)
+        self.operation_keys_frame = Frame(self, bg="#00E6BF", width=200, height=250)
+
+        self.numeric_keys_frame.columnconfigure(0, minsize=133)
+        self.numeric_keys_frame.columnconfigure(1, minsize=133)
+        self.numeric_keys_frame.columnconfigure(2, minsize=133)
+
+        self.operation_keys_frame.columnconfigure(0, minsize=133)
+        self.operation_keys_frame.columnconfigure(1, minsize=133)
+        self.operation_keys_frame.columnconfigure(2, minsize=133)
+
+        self.display_frame.pack()
+        self.numeric_keys_frame.pack(side='left')
+        self.operation_keys_frame.pack(side='right')
+    
+    def numeric_buttons_organization(self):
+        keygrid = [3, 0]
+
+        for b in range(-2, 10):
+            if keygrid[1] == 2:
+                keygrid[0] -= 1
+                keygrid[1] = 0
+            else:
+                keygrid[1] += 1
+
+
+test = Application()
+test.mainloop()
