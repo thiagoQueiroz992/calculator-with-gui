7354dff96cd2add4a7646dd3ef900516d770f12f
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-04-24 09:49:36 -0300

    Numeric buttons made

--- main.py
+++ main.py
@@ -32,22 +32,37 @@ class Application(Tk):
         self.numeric_keys_frame = Frame(self, bg="#FCFF67", width=200, height=250)
         self.operation_keys_frame = Frame(self, bg="#00E6BF", width=200, height=250)
 
-        self.numeric_keys_frame.columnconfigure(0, minsize=133)
-        self.numeric_keys_frame.columnconfigure(1, minsize=133)
-        self.numeric_keys_frame.columnconfigure(2, minsize=133)
+        self.numeric_keys_frame.columnconfigure(0, minsize=66)
+        self.numeric_keys_frame.columnconfigure(1, minsize=66)
+        self.numeric_keys_frame.columnconfigure(2, minsize=66)
 
-        self.operation_keys_frame.columnconfigure(0, minsize=133)
-        self.operation_keys_frame.columnconfigure(1, minsize=133)
-        self.operation_keys_frame.columnconfigure(2, minsize=133)
+        self.operation_keys_frame.columnconfigure(0, minsize=66)
+        self.operation_keys_frame.columnconfigure(1, minsize=66)
+        self.operation_keys_frame.columnconfigure(2, minsize=66)
 
         self.display_frame.pack()
         self.numeric_keys_frame.pack(side='left')
         self.operation_keys_frame.pack(side='right')
+
+        self.numeric_buttons_organization()
     
     def numeric_buttons_organization(self):
         keygrid = [3, 0]
 
         for b in range(-2, 10):
+            if b == -2:
+                self.buttons.append(Button(self.numeric_keys_frame, text='+/-', width=6, height=3, padx=4, pady=4))
+                self.buttons[-1].grid(row=keygrid[0], column=keygrid[1])
+            elif b == -1:
+                self.buttons.append(Button(self.numeric_keys_frame, text='0', width=6, height=3, padx=4, pady=4))
+                self.buttons[-1].grid(row=keygrid[0], column=keygrid[1])
+            elif b == 0:
+                self.buttons.append(Button(self.numeric_keys_frame, text=',', width=6, height=3, padx=4, pady=4))
+                self.buttons[-1].grid(row=keygrid[0], column=keygrid[1])
+            else:
+                self.buttons.append(Button(self.numeric_keys_frame, text=str(b), width=6, height=3, padx=4, pady=4))
+                self.buttons[-1].grid(row=keygrid[0], column=keygrid[1])
+
             if keygrid[1] == 2:
                 keygrid[0] -= 1
                 keygrid[1] = 0
