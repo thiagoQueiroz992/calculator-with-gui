5a29c6208a2e11178cdd64e69d2e8b52869e9f3d
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-04-24 09:00:40 -0300

    Created init and basic operating methods

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 class Calculator:
-    pass
+    def __init__(self):
+        self.operators = {
+            0: 'Summing',
+            1: 'Subtraction',
+            2: 'Multiplication',
+            3: 'Division'
+        }
+    
+    def sum(self, a: float, b: float) -> float:
+        return a + b
+    
+    def subtract(self, a: float, b: float) -> float:
+        return a - b
+    
+    def multiplicate(self, a: float, b: float) -> float:
+        return a * b
+    
+    def divide(self, a: float, b: float) -> float:
+        return a / b
